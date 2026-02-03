71930f5a9da87038b7ce9d60c0c297f4e3ce729c
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-02-03 08:50:36 +0530

    Time: 37 ms (19.4%), Space: 48.2 MB (75.13%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,35 +1,28 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         Map<Character,Integer> freq = new HashMap<>();
-        for(char task:tasks){
+        for(char task : tasks){
             freq.put(task,freq.getOrDefault(task,0)+1);
         }
-
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int count : freq.values()){
             pq.add(count);
         }
-
         int time =0;
-
         while(!pq.isEmpty()){
-            List<Integer> temp = new ArrayList<>();
             int cycle = n+1;
             int i=0;
-            
+            List<Integer> temp = new ArrayList<>();
             while(i<cycle && !pq.isEmpty()){
                 int cnt = pq.poll();
                 cnt--;
                 if(cnt>0) temp.add(cnt);
-
                 time++;
                 i++;
             }
-
-            for(int rem:temp){
+            for(int rem: temp){
                 pq.add(rem);
             }
-
             if(!pq.isEmpty()){
                 time += (cycle-i);
             }
