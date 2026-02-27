b25d1088d15bb229a17a286c2224a7ad3ba1e81d
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-02-27 10:19:43 +0530

    Time: 4 ms (73.47%), Space: 96.7 MB (31.78%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,36 +9,43 @@
  * }
  */
 class Solution {
-    private ListNode reverse(ListNode head){
-            ListNode temp = head;
-            ListNode prev = null;
-            while(temp!=null){
-                ListNode front = temp.next;
-                temp.next=prev;
-                prev=temp;
-                temp=front;
-            }
-            return prev;
+    private static ListNode reverse(ListNode head){
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp!=null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
         }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow = slow.next;
-            fast=fast.next.next;
-        }
-        ListNode newHead = reverse(slow.next);
-        ListNode first = head;
-        ListNode second = newHead;
-        while(second!=null){
-            if(first.val!=second.val){
-                reverse(newHead);
-                return false;
-            }
-            first=first.next;
-            second=second.next;
-        }
-        reverse(newHead);
+       if(head==null || head.next==null){
         return true;
+       }
+       ListNode fast = head;
+       ListNode slow = head;
+
+       while(fast.next!=null && fast.next.next!=null){
+        fast = fast.next.next;
+        slow = slow.next;
+       }
+
+       ListNode newHead = reverse(slow.next);
+
+       ListNode first = head;
+       ListNode second = newHead;
+
+       while(second!=null){
+        if(first.val!=second.val){
+            newHead = reverse(newHead);
+            return false;
+        }
+        first = first.next;
+        second = second.next;
+       }
+       newHead = reverse(newHead);
+       return true;
     }
 }
