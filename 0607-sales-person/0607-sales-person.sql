cb274c7acf854ad983c4878ee7b929274a9d66f7
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-08-30 13:17:45 +0530

    Time: 1627 ms (46.1%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,11 +1,10 @@
 # Write your MySQL query statement below
-select name
-from SalesPerson
-where sales_id not in(
-    select
-        o.sales_id
-        from Orders o
-        join Company c
-        on o.com_id = c.com_id
-        where c.name = 'RED'
-)
+select
+    s.name
+from SalesPerson s
+left join Orders o
+on s.sales_id = o.sales_id
+left join Company c
+on c.com_id = o.com_id and c.name = 'RED'
+group by s.sales_id
+having count(c.com_id) =0
