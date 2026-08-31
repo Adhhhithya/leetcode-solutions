72632146150ac3f1c8a2fc764c1d7df66a509cf1
Adhithya J <162481176+Adhhhithya@users.noreply.github.com>
2026-08-31 12:02:32 +0530

    Time: 373 ms (59.38%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select
-    m.name 
+    m.name
 from Employee m
-join Employee e
-on m.id = e.managerId
+left join Employee e
+on e.managerId = m.id
 group by m.id
-having count(*) >= 5
+having count(*) >=5
